44af7329a99d0ef029609848e84699bf696dd2dd
Eric Company <ericcovi@hotmail.com>
2022-05-16 17:40:43 +0200

    Add missile launching, collision with obstacles, item droping and life meter

--- js/index.js
+++ js/index.js
@@ -1,83 +1,179 @@
 const canvas = document.getElementById("canvas");
-const gameIntro = document.querySelector(".game-screen");
+const gameIntro = document.querySelector(".game-start");
+const runningGame = document.querySelector(".running-game");
 const titleText = document.getElementById("space-invaders-title");
 const ctx = canvas.getContext("2d");
 const startGameBtn = document.getElementById("start-button");
+const gameOverScreen = document.querySelector(".game-over")
 
 const bgImage = new Image();
 bgImage.src = "../images/pixel-art-stellar-background.jpg";
-const playerImg = new Image();
-playerImg.src = "../images/playerShip3_orange.png";
+const playerImage = new Image();
+playerImage.src = "../images/playerShip3_orange.png";
 const brownMeteorImage = new Image();
 brownMeteorImage.src = "../images/meteorBrown_big4.png";
+const destroyedBrownMeteor = new Image();
+destroyedBrownMeteor.src = "";
+const missileImage = new Image();
+missileImage.src = "../images/laserRed02.png";
+const lifeImage = new Image();
+lifeImage.src = "../images/hearth.png.png";
+const lifePlusDrop = new Image();
+lifePlusDrop.src = "../images/pill_red.png";
 
 const playerHeight = 60;
 const playerWidth = 60;
 let playerX = canvas.width / 2 - playerWidth / 2;
 let playerY = canvas.height - 200;
 let playerSpeed = 3;
-let obstSpeed = 2;
-let obstW = 70;
-let obsHeight = 70;
 
 let playerIsGoingLeft = false;
 let playerIsGoingRight = false;
 let playerIsGoingUp = false;
 let playerIsGoingDown = false;
-let playerIsShooting = false
+let playerIsShooting = false;
+
+let obstacles = [];
+let obstWidth = 70;
+let obstHeight = 70;
+let gravity = 2;
+
+let missiles = [];
+let missileWidth = 10;
+let missileHeight = 10;
+let missileSpeed = playerSpeed + 0.5;
+let laserInCooldown = false;
+
+let drops = [];
+let dropWidth = 22;
+let dropHeight = 20;
+let dropSpeed = gravity;
 
 let animationFrameId;
 let gameOver = false;
+let score = 0;
 
-let obstacles = [];
-
+class Player {
+  constructor() {
+    this.xPos = playerX;
+    this.yPos = playerY;
+    this.speed = playerSpeed;
+    this.width = playerWidth;
+    this.height = playerHeight;
+    this.health = 3;
+  }
+}
 class Obstacle {
   constructor() {
-    this.width = obstW;
-    this.height = obsHeight;
-    this.speed = obstSpeed;
-    this.xPos = Math.floor(Math.random() * (canvas.width-this.width));
+    this.width = obstWidth;
+    this.height = obstHeight;
+    this.speed = gravity;
+    this.xPos = Math.floor(Math.random() * (canvas.width - this.width));
     this.yPos = 0;
+    this.destroyed = false;
+    this.chanceToDrop = 1 + Math.floor(Math.random() * 100);
   }
-
   move() {
     this.yPos += this.speed;
   }
 }
 
+class Missile {
+  constructor() {
+    this.width = missileWidth;
+    this.height = missileHeight;
+    this.speed = missileSpeed;
+    this.xPos = player.xPos + player.width / 2;
+    this.yPos = player.yPos - 10;
+    this.destroyed = false;
+  }
+  move() {
+    this.yPos -= this.speed;
+  }
+}
+
+class Drop {
+  constructor(destroyedObstacleX, destroyedObstacleY) {
+    this.width = dropWidth;
+    this.height = dropHeight;
+    this.speed = dropSpeed;
+    this.xPos = destroyedObstacleX + obstWidth / 2;
+    this.yPos = destroyedObstacleY + obstHeight / 2;
+    this.destroyed = false;
+  }
+  move() {
+    this.yPos += this.speed;
+  }
+}
 function drawPlayer() {
-  ctx.drawImage(playerImg, playerX, playerY, playerWidth, playerHeight);
-  console.log("player printed");
+  ctx.drawImage(
+    playerImage,
+    player.xPos,
+    player.yPos,
+    player.width,
+    player.height
+  );
+  for (let i = 0; i < player.health; i += 1) {
+    ctx.drawImage(lifeImage, 20 * i, 0, 50, 50);
+  }
+
   if (playerIsGoingLeft) {
-    if (playerX > 0) {
-      playerX -= playerSpeed;
+    if (player.xPos > 0) {
+      player.xPos -= player.speed;
     }
   }
   if (playerIsGoingRight) {
-    if (playerX + playerWidth < canvas.width) {
-      playerX += playerSpeed;
+    if (player.xPos + player.width < canvas.width) {
+      player.xPos += player.speed;
     }
   }
 
   if (playerIsGoingUp) {
-    if (playerY > 0) {
-      playerY -= playerSpeed;
+    if (player.yPos > 0) {
+      player.yPos -= player.speed;
     }
   }
   if (playerIsGoingDown) {
-    if (playerY + playerHeight < canvas.height) {
-      playerY += playerSpeed;
+    if (player.yPos + player.height < canvas.height) {
+      player.yPos += player.speed;
     }
   }
 }
 
+function collision(a, b) {
+  let collisionChecked = false;
+  if (
+    a.xPos < b.xPos + b.width &&
+    a.xPos + a.width > b.xPos &&
+    a.yPos < b.yPos + b.height &&
+    a.height + a.yPos > b.yPos
+  ) {
+    // ¡colision detected!
+    collisionChecked = true;
+  }
+  return collisionChecked;
+}
+
 function drawObstacle(obstacle) {
-  const { xPos, yPos, width, height } = obstacle; 
-  console.log(obstacle)
+  const { xPos, yPos, width, height } = obstacle;
 
   obstacle.move();
 
   ctx.drawImage(brownMeteorImage, xPos, yPos, width, height);
+}
+
+function drawMissile(missile) {
+  const { xPos, yPos, width, height } = missile;
+
+  missile.move();
+  ctx.drawImage(missileImage, xPos, yPos, width, height);
+}
+
+function drawDrop(drop) {
+  const { xPos, yPos, width, height } = drop;
+
+  drop.move();
+  ctx.drawImage(lifePlusDrop, xPos, yPos, width, height);
 }
 
 function animate() {
@@ -86,47 +182,90 @@ function animate() {
   drawPlayer();
   const nextObstacles = [];
   obstacles.forEach((obstacle) => {
-    console.log("new obs");
+    if (collision(player, obstacle)) {
+      obstacle.destroyed = true;
+      player.health -= 1;
+      console.log(player.health)
+      console.log(animationFrameId)
+      if (player.health === 0) {
+        gameOver = true;
+      }
+
+    }
+    missiles.forEach((missile) => {
+      if (collision(obstacle, missile)) {
+        obstacle.destroyed = true;
+        missile.destroyed = true;
+        if (obstacle.chanceToDrop > 75) {
+          drops.push(new Drop(obstacle.xPos, obstacle.yPos));
+        }
+        score += 10;
+      }
+    });
     if (
-      playerX < obstacle.xPos + obstacle.width &&
-      playerX + playerWidth > obstacle.xPos &&
-      playerY < obstacle.yPos + obstacle.height &&
-      playerHeight + playerY > obstacle.yPos
+      obstacle.yPos < canvas.height + obstacle.height &&
+      !obstacle.destroyed
     ) {
-      gameOver = true;
-      console.log("Collision!")
-    } 
-    
-
-    drawObstacle(obstacle);
-    if (obstacle.yPos > canvas.height + obstacle.yPos) {
       nextObstacles.push(obstacle);
-
+      drawObstacle(obstacle);
       obstacles = nextObstacles;
     }
   });
-  
+
+  const nextMissiles = [];
+  missiles.forEach((missile) => {
+    if (missile.yPos > 0 - missile.height && !missile.destroyed) {
+      nextMissiles.push(missile);
+      drawMissile(missile);
+      missiles = nextMissiles;
+    }
+  });
+
+  const nextDrops = [];
+  drops.forEach((drop) => {
+    if (collision(player, drop)) {
+      drop.destroyed = true
+      if(player.health<3) {
+        player.health +=1
+      }
+    }
+    if (drop.yPos > 0 - drop.height && !drop.destroyed) {
+      nextDrops.push(drop);
+      drawDrop(drop);
+      drops = nextDrops;
+    }
+  });
   if (animationFrameId % 75 === 0) {
     obstacles.push(new Obstacle());
   }
 
-  if(animationFrameId % 500 == 0){
-    obstSpeed += 2
-    playerSpeed += 1
-    
+  if (playerIsShooting && !laserInCooldown) {
+    laserInCooldown = true;
+    setTimeout(() => {
+      laserInCooldown = false;
+    }, 350);
+    missiles.push(new Missile());
+  }
+
+  if (animationFrameId % 500 == 0) {
+    gravity += 2;
+    playerSpeed += 1;
   }
   if (gameOver) {
     cancelAnimationFrame(animationFrameId);
+    runningGame.style.display = "none"
+    gameOverScreen.style.display = "block"
+
   } else {
-    console.log(animationFrameId);
     animationFrameId = requestAnimationFrame(animate);
+    console.log({obstacles})
   }
 }
 
 function startGame() {
-  startGameBtn.style.display = "none";
-  titleText.style.display = "none";
-  canvas.style.display = "block";
+  gameIntro.style.display = "none";
+  runningGame.style.display = "block";
+
   animate();
 }
 
@@ -164,5 +303,10 @@ window.addEventListener("load", () => {
     if (event.code === "ArrowDown") {
       playerIsGoingDown = false;
     }
+    if (event.code === "Space") {
+      playerIsShooting = false;
+    }
   });
 });
+
+const player = new Player();
