de2c60f0e0f7e3df13bf8f4cea200fa88423bfa2
Eric Company <ericcovi@hotmail.com>
2022-05-19 14:19:28 +0200

    Fixing loading errors 3

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ enemyImage.src = "./images/enemyBlue2.png";
 const pauseScreenImage = new Image();
 pauseScreenImage.src = "./images/pause-screen.png";
 
-let gameAudio = new Audio("../audio/running game audio.mp3");
+let gameAudio = new Audio("./audio/running game audio.mp3");
 gameAudio.volume = 0.1;
 
 const playerHeight = 60;
